8a59accebb9b8dc222ca1e5641d6703dfe4148b5
Benjamin Benoit <benjaminbenoit@MacBook-Pro-de-Benjamin.local>
2023-02-13 16:31:32 +0100

    fonctions

--- index.php
+++ index.php
@@ -1,5 +1,31 @@
 <?php
 require_once "vendor/autoload.php";
 
-use Benjaminbenoit\Composer\Personnage;
+use BenjaminChristelle\CombatGamePoo\Personnages;
 
+$hero = new Personnages();
+$hero->name = "Christelle";
+$hero->life = 200;
+$hero->attack = 15;
+
+$minion = new Personnages();
+$minion->name = "Minion";
+$minion->life = 10;
+$minion->attack = 10;
+
+$lieutenantMinion = new Personnages();
+$lieutenantMinion->name = "Lieutenant Minion";
+$lieutenantMinion->life = 30;
+$lieutenantMinion->attack = 30;
+
+$chefMinion = new Personnages();
+$chefMinion->name = "Chef Minion";
+$chefMinion->life = 100;
+$chefMinion->attack = 100;
+
+$hero->lifeBonus();
+$hero->armorBonus();
+
+$minion->randomAttack($hero);
+
+dump($hero, $minion, $lieutenantMinion, $chefMinion);
--- src/Personnages.php
+++ src/Personnages.php
@@ -1,4 +1,28 @@
 <?php
 
-namespace Benjaminbenoit\Composer;
+namespace BenjaminChristelle\CombatGamePoo;
 
+class Personnages {
+    public $name;
+    public $life;
+    public $armor;
+    public $attack;
+
+    public function lifeBonus() {
+        $this->life = $this->life +50;
+    }
+
+    public function armorBonus() {
+        $this->armor = $this->armor +20;
+    }
+
+    public function randomAttack($cible) {
+        $random = rand(0, 1);
+        if($random == 1) {
+            echo "Succès";
+            $cible->life = $cible->life - ($this->attack - $cible->armor);
+        } else {
+            echo "Echec";
+        }
+    }
+}
